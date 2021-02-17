00780d9b7adc9eebc84267c9325e5da2d432417d
samfan01 <dsamfan@gmail.com>
2021-02-17 14:40:30 +0300

    added the not found route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {path: 'user', component:UserComponent},
   {path: 'about', component:AboutComponent},
+  {path:'**',component:NotFoundComponent},
+  { path: '', redirectTo:"/user", pathMatch:"full"},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import {FormsModule} from '@angular/forms';
 import { AboutComponent } from './about/about.component';
-import { NavbarComponent } from './navbar/navbar.component'
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     AboutComponent,
-    NavbarComponent
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
