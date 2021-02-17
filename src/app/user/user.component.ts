b79c922a322512582ece9f6ef95696c5e2c25087
samfan01 <dsamfan@gmail.com>
2021-02-17 13:33:09 +0300

    added get users function to fetch user info

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../user';
 import {Repo} from '../repo';
-import
+import {GitserviceService} from '../gitservice.service'
 
 
 @Component({
@@ -10,10 +10,31 @@ import
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  
+  user =new User('Samfan01','',0)
+  username = User.name
 
-  constructor() { }
+  repos: any = [];
+
+  constructor(private gitService:GitserviceService) { }
+
+  profile:any;
 
   ngOnInit(): void {
+    this.getUsers();
+  }
+  getUsers():any {
+    interface ApiResponse {
+      login:string;
+      avatar_url:string;
+      followers:number;
+      following:number;
+      pubic_reepos:number;
+      location:string;
+    }
+    this.gitService.getData(this.user.name).subscribe(data=>{
+      this.profile = data;
+    })
   }
 
 }
