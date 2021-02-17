96591dfcff971a94335d38e387ac81c8b2bc48ea
samfan01 <dsamfan@gmail.com>
2021-02-17 13:14:54 +0300

    created the user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,10 @@
 export class User {
+    constructor(
+        public login?:string,
+        public avatar_url?:string,
+        public followers?:number,
+        public following?:number,
+        public public_repos?:number,
+        public location?:string
+    ){}
 }
